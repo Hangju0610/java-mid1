08ef16f741b620aa225f87678b64e0b3dc7d654d
Hangju0610 <131460159+Hangju0610@users.noreply.github.com>
2024-07-19 23:46:44 +0900

    wrapper basic 1

---
+++ src/lang/wrapper/MyInteger.java
@@ -0,0 +1,33 @@
+package lang.wrapper;
+
+/**
+ * 캡슐화 진행
+ * 다양한 기능 추가
+ */
+public class MyInteger {
+
+    private final int value;
+
+    public MyInteger(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public int compareTo(int target) {
+        if (value < target) {
+            return -1;
+        } else if (value > target) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value); // 숫자를 문자로 변경
+    }
+}
