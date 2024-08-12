878a67b30834a6bc5a3abed4a4358c0806460281
Hangju0610 <131460159+Hangju0610@users.noreply.github.com>
2024-08-12 21:23:46 +0900

    Local class init

--- src/enumeration/ref3/Grade.java
+++ src/enumeration/ref3/Grade.java
@@ -3,8 +3,11 @@ package enumeration.ref3;
 public enum Grade {
     BASIC(10), GOLD(20), DIAMOND(30);
 
+    // class처럼 내부 맴버를 가질 수 있다.
     private final int discountPercent;
 
+    // 또한 생성자도 사용이 가능하다.
+    // 단, 생성자에 접근이 불가능하도록 막혀있다. private이라고 생각하면 편하다.
     Grade(int discountPercent) {
         this.discountPercent = discountPercent;
     }
@@ -16,6 +19,4 @@ public enum Grade {
     public int discount(int price) {
         return price * discountPercent / 100;
     }
-
-
 }
